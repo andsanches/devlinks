5c203786464dfc25b07a55048f8a9ae15ce20657
anderson.sanches <andersonsanches.adm@gmail.com>
2023-01-03 23:08:27 -0300

    change image and links

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.jpeg")
     img.setAttribute(
       "alt",
       "Foto de Mayk Brito de óculos, sorrindo, com fundo azul e rosa"
     )
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.jpeg")
     img.setAttribute(
       "alt",
       "Foto de Mayk Brito sorrindo, com fundo azul e rosa"
